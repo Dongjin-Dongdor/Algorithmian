3f4fcb6dcb2798559ffdad2d2332b3b07431e245
dongdor <djinkim0413@gmail.com>
2016-07-06 19:04:59 +0900

    DoubleLinkedList

---
+++ Algorithmian_DoubleLinkedList/Algorithmian_DoubleLinkedList/DoubleLinkedList.c
@@ -0,0 +1,9 @@
+//
+//  DoubleLinkedList.c
+//  Algorithmian_DoubleLinkedList
+//
+//  Created by 김동진 on 2016. 7. 6..
+//  Copyright © 2016년 김동진. All rights reserved.
+//
+
+#include "DoubleLinkedList.h"
---
+++ Algorithmian_DoubleLinkedList/Algorithmian_DoubleLinkedList/DoubleLinkedList.h
@@ -0,0 +1,22 @@
+//
+//  Header.h
+//  Algorithmian_DoubleLinkedList
+//
+//  Created by 김동진 on 2016. 7. 6..
+//  Copyright © 2016년 김동진. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+typedef struct tagNode
+{
+    int Data;
+    Struct tagNode* PrevNode;
+    Struct tagNode* NextNode;
+    
+}Node;
+
+
+
+#endif /* Header_h */
