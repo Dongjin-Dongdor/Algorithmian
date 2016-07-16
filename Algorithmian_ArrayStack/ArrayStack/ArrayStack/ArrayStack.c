84646839a9c8ccf4037328392c14281b4a575bd6
dongdor <djinkim0413@gmail.com>
2016-07-16 12:23:18 +0900

    mkdir for Algo

--- Algorithmian_ArrayStack/ArrayStack/ArrayStack/ArrayStack.c
+++ Algorithmian_ArrayStack/ArrayStack/ArrayStack/ArrayStack.c
@@ -6,10 +6,52 @@
 //  Copyright © 2016년 김동진. All rights reserved.
 //
 
-#include <stdio.h>
+#include "ArrayStack.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+/* 스택의 생성과 소멸 */
+
+void AS_CreateStack(ArrayStack** Stack, int Capacity)
+{
+    //스택을 자유 저장소에 생성
+    (*Stack) = (ArrayStack*)malloc(sizeof(ArrayStack));
+    
+    //입력된 Capacity만큼의 노드를 자유 저장소에 생성
+    (*Stack)->Nodes = (Node*)malloc(sizeof(Node)*Capacity);//*Cappacity사이즈 만큼의 공간을 받게됨
+    
+    // Capacity및 Top초기화
+    (*Stack)->Capacity = Capacity;
+    
+    (*Stack)->Top = 0;//Top은 노드가 쌓일때마다 1씩 증가한다.
 }
+
+
+void AS_DestroyStack(ArrayStack* Stack)
+{
+    /*노드를 자유저장소에서 해제 */
+    free(Stack->Nodes);
+    
+    /*스택을 자유 저장소에서 해제 */
+    free(Stack);
+}
+
+
+/* 삽입(Push) 연산 */
+// (*Stack).Data 를 줄여서 Stack->Data로 표시한다.
+void AS_Push(ArrayStack* Stack, ElementType Data)
+{
+    int Position = Stack ->Top;
+    Stack->Nodes[Position].Data = Data;
+    Stack->Top++;
+}
+
+/* 제거 */
+/* 탑이 가지고 있는 값은 실제 최상위 노드가 배열 내에서 위치하는 인덱스 값보다 1만큼 더 크다는 것이다.*/
+ElementType AS_Pop(ArrayStack* Stack)
+{
+    int Position = --(Stack ->Top);
+    
+    return Stack->Nodes[Position].Data;
+    
+}
+
--- Algorithmian_ArrayStack/ArrayStack/ArrayStack/ArrayStack.h
+++ Algorithmian_ArrayStack/ArrayStack/ArrayStack/ArrayStack.h
@@ -9,5 +9,49 @@
 #ifndef ArrayStack_h
 #define ArrayStack_h
 
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef int ElementType;
+
+/*
+스택의 노드는 다음과같이 데이터만 담는 구조체로 표현된다.
+노드의 위치는 배열의 인덱스로 알 수 있기 때문에 링크드 리스트처럼 앞이나 뒷 노드에 대한 포인터는 필요없다.
+ */
+typedef struct tagNode
+{
+    ElementType Data;
+    
+}Node;
+
+
+/*
+배열기반 스택의 구조체는 [용량],[최상위 노드의 위치],[노드 배열]을 가지고 있다.
+*/
+
+typedef struct tagArrayStack
+{
+    int Capacity; //용량
+    int Top; //최상위 노드의 위치
+    Node* Nodes; // 노드
+    
+}ArrayStack;
+
+
+void AS_CreateStack(ArrayStack** Stack, int Capacity);
+
+void AS_DestroyStack(ArrayStack* Stack);
+
+void AS_Push(ArrayStack* Stack, ElementType Data);
+
+ElementType AS_Pop(ArrayStack* Stack);
+
+ElementType AS_Top(ArrayStack* Stack);
+
+int AS_GetSize(ArrayStack* Stack);
+
+int AS_IsEmpty(ArrayStack* Stack);
+
 
 #endif /* ArrayStack_h */
