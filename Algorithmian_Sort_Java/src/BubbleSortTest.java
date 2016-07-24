1d0bf25e1d5cab02b35a8028fe6aa1a32c130a30
dongdor <djinkim0413@gmail.com>
2016-07-24 20:07:02 +0900

    no message

--- Algorithmian_Sort_Java/src/BubbleSortTest.java
+++ Algorithmian_Sort_Java/src/BubbleSortTest.java
@@ -9,7 +9,12 @@
  */
 public class BubbleSortTest {
     public static void main(String[] args){
-
+        int inputArray[] = {4,6,7,1,3,18,16,15,15,20,9};
+        System.out.println("Before Sort : PrintArray()");
+        printArray(inputArray);
+        bubbleSort(inputArray);
+        System.out.println("Alter Sort : PrintArray()");
+        printArray(inputArray);
 
     }
 
@@ -24,9 +29,18 @@ public class BubbleSortTest {
     final은 상속 불가를 나타내는데 abstract는 추상 클래스를 생성할 때 사용하는 내용이므로 서로 배타적이 된다
 
     final메소드
+    메소드에 final을 선언하게 되면 그 메소드가 포함 클레스를 상속받은 클래스에서 오라이딩으로 재정의 할 수 없는 메소드.
      */
     //
+    /*
+    static 변수(정적변수)
+    static이 붙은 변수는 클래스의 모든 객체가 공유할 수 있다
+
+    static 메소드(정적변수)
+    인스턴스 생성없이 호출 가능하다
+     */
     //static 은 공용의 변수를 만들 때 사용되는 예약어
+
     public static final void bubbleSort(int input[]){
         for(int i = 0; i<input.length-1;i++){
             for(int j = 0; j<input.length-i-1;j++){
@@ -40,5 +54,12 @@ public class BubbleSortTest {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
+    }
+
+    public static final void printArray(int array[]){
+        for(int element:array){
+            System.out.print(element+"");
+        }
+        System.out.println("");
     }
 }
--- Algorithmian_Sort_Java/src/SelectSortTest.java
+++ Algorithmian_Sort_Java/src/SelectSortTest.java
@@ -1,8 +1,12 @@
 /**
  * Created by dongdor on 2016. 7. 23..
  */
+
 public class SelectSortTest {
     public static void main(String[] args){
 
     }
+
+    public static void SelectSort(int  )
+
 }
