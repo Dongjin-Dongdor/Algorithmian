3a5a3a8e2c1392900c290bf3cb0d1c130aac177c
dongdor <djinkim0413@gmail.com>
2016-07-29 20:51:57 +0900

    no message

--- Algorithmian_QuickSelect_Java/src/QuickSelectTest.java
+++ Algorithmian_QuickSelect_Java/src/QuickSelectTest.java
@@ -5,6 +5,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 /**
 퀵 셀렉트는 N개의 원소를 정렬하는 것이 아니라 K 번재 숫자만 알고싶은 경우에 사용하는 방법이다
@@ -43,13 +44,58 @@ public class QuickSelectTest {
     }
 
     private static Integer quickSelect(int[] array, int k){
-        
+
         int left = 0;
         int right = array.length - 1;
+        Random rand = new Random();
+        while(right >= left){
+            int pivotIndex = partition(array, left, right,
+                    rand.nextInt(right-left+1)+left);
+            if(pivotIndex == k){
+                return array[pivotIndex];
+            }else if(pivotIndex<k){
+                left = pivotIndex + 1;
+            }else if(pivotIndex>k){
+                right = pivotIndex - 1;
+            }
 
+        }
+        return null;
+    }
+
+    private static int partition(int[] array, int left, int right, int pivot){
+        int pivotVal = array[pivot];
+        swap(array, pivot, right);
+        int storeIndex = left;
+        for(int i = left; i<right;i++){
+            if(array[i]<pivotVal){
+                swap(array, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        swap(array, right, storeIndex);
+        return storeIndex;
+    }
+
+    public static void swap(int[] array, int index1, int index2){
+
+        int temp  = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+
+    }
+
+    private static int[] stringArrayToIntArray(String[] inputs){
+        int[] outputs = new int[inputs.length];
+        for(int i = 0; i< inputs.length; i++){
+            outputs[i] = Integer.parseInt(inputs[i]);
+        }
+        return outputs;
     }
 }
 
-
+/**
+ *
+ */
 
 
