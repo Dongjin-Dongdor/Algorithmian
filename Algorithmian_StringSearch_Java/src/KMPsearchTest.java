1cc7fb12d1b2701c707e1ef107cdcea773ae90a8
dongdor <djinkim0413@gmail.com>
2016-08-23 23:05:25 +0900

    문자열찾기 완료

--- Algorithmian_StringSearch_Java/src/KMPsearchTest.java
+++ Algorithmian_StringSearch_Java/src/KMPsearchTest.java
@@ -5,6 +5,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.List;
+import java.util.LinkedList;
 /**
  1. 일반적인 검색
  - 본문 문자열을 일일히 비교하는 방식
@@ -52,8 +53,8 @@ public class KMPsearchTest {
         //next()메소드의 경우 첫 단어 앞쪽 공백이나 개행문자는 무시하고 하나의 단어를 입력받고
         //단어뒤의 개행문자는 그대로 놔둔다
         //nextLine()메소드의 경우 개행문자까지를 한줄로 인식하고 입력을 받고 \n는 버려진다
-        char[] T = sc.nextLine().toCharArray();
-        char[] P = sc.nextLine().toCharArray();
+        char[] T = sc.nextLine().toCharArray(); // 검색할 문자열
+        char[] P = sc.nextLine().toCharArray(); // 검색 패턴
 
         List result = kmpSearch(T,P);
         System.out.println(result.size());
@@ -63,9 +64,54 @@ public class KMPsearchTest {
 
     }
 
-    public static int[] getMaxBoundaryWidthArray(char[] P){
-        int[] maxBoundaryWidthArray = new int[]
+    //P는 검색할 패턴이다
+    public static int[] getMaxBoundaryWidthArray(char[] P){ //검색할 패턴을 인자로 보내준다
+        int[] maxBoundaryWidthArray = new int[ P.length + 1];  //마지막 최대너비까지 구해준다
+        for(int i = 0; i <= P.length; i++){
+            if(i == 0){
+                maxBoundaryWidthArray[i] = -1;
+                continue;
+            }
+            int length = i / 2;
+            int max = i - 1;
+            int count = 0;
+            while(length > 0){
+                if(P[(length - 1)] == P[max]){
+                    count += 1;
+                    max -= 1;
+                }
+                length -= 1;
+            }
+            maxBoundaryWidthArray[i] = count;
+        }
+        for(int i = 0; i<maxBoundaryWidthArray.length-1; i++){
+            System.out.println(" : "+maxBoundaryWidthArray[i]);
+        }
+        return maxBoundaryWidthArray;
     }
 
+    public static List kmpSearch(char[] T, char[] P){
+        List matchPrefixIndexResult = new LinkedList<Integer>(); //결과값
+        int[] maxBoundaryWidthArray = getMaxBoundaryWidthArray(P);
+        int currentIndex = 0;
+        while((currentIndex + P.length) <= T.length){
+
+            int prefixIndex = 0;
+            while(prefixIndex < P.length){
+                if(T[currentIndex + prefixIndex] == P[prefixIndex]){
+                    prefixIndex += 1;
+                }
+                else{
+                    break;
+                }
+            }
+            if(prefixIndex == P.length){
+                matchPrefixIndexResult.add(currentIndex + 1);
+            }
+            int move = prefixIndex - maxBoundaryWidthArray[prefixIndex];
+            currentIndex += move;
+        }
+        return matchPrefixIndexResult;
+    }
 
 }
